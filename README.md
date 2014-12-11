564ac27fc15b4c9f0eec4561e9f1310bb0ab1b35
Alexander Shereminskiy <aalex.prog@gmail.com>
2014-12-12 00:02:36 +0200

    Added vagrand instalation

---
+++ README.md
@@ -0,0 +1,14 @@
+Instructions
+If you
+
+run Mac OS X or Linux
+have node 0.11.14 installed ($ node -v prints 0.11.14)
+can bulid binary Node packages (npm install ws doesn't fail)
+then you can skip all the steps below.
+
+Otherwise, do the following:
+
+Install VirtualBox
+Install Vagrant
+Create a folder, place Vagrantfile and setup.sh into it
+Execute vagrant up, it will take a while. Make sure that it prints 0.11.14 at the end.
---
+++ setup.sh
@@ -0,0 +1,20 @@
+sudo locale-gen UTF-8
+echo '================================================='
+echo 'Starting up...'
+echo '================================================='
+echo 'Updating packages...'
+echo '================================================='
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+echo '================================================='
+echo 'Installing Node-Stable...'
+echo '================================================='
+sudo apt-get install -y --force-yes build-essential nodejs
+echo '================================================='
+echo 'Installing Node-Latest...'
+echo '================================================='
+sudo npm install -g n
+sudo n 0.11.14
+echo '================================================='
+echo 'All Done!'
+echo '================================================='
+node -v
